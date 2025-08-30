e10c4f69e6945c85120a824155b934fdbc0c7b9a
Lamouchemasquee <132230709+Jessimouche@users.noreply.github.com>
2025-08-30 09:31:33 -0400

    Add files via upload

---
+++ src/__Class.lua
@@ -0,0 +1,13 @@
+__Class = Class{}
+
+function __Class:init()
+
+end
+
+function __Class:update(dt)
+
+end
+
+function __Class:render()
+
+end
---
+++ src/constants.lua
@@ -0,0 +1,9 @@
+WINDOW = {
+    ["WIDTH"] = 1280,
+    ["HEIGHT"] = 720,
+}
+
+VIRTUAL = {
+    ["WIDTH"] = ,
+    ["HEIGHT"] = ,
+}
