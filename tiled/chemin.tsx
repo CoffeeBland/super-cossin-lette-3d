2c42295c0d432ff22641a821ea98f18fc4f362bd
Lamouchemasquee <jessicomics@gmail.com>
2025-08-30 20:17:13 -0400

    Update chemin.tsx

--- tiled/chemin.tsx
+++ tiled/chemin.tsx
@@ -1,50 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="chemin" tilewidth="128" tileheight="74" tilecount="30" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="Chemin" tilewidth="128" tileheight="74" tilecount="30" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="Tiles/Tuile_chemin-1.png" width="128" height="74"/>
+ </tile>
  <tile id="1">
   <image source="Tiles/Tuile_chemin-2.png" width="128" height="74"/>
  </tile>
- <tile id="9">
-  <image source="Tiles/Tuile_chemin-10.png" width="128" height="74"/>
- </tile>
- <tile id="10">
-  <image source="Tiles/Tuile_chemin-11.png" width="128" height="74"/>
+ <tile id="2">
+  <image source="Tiles/Tuile_chemin-3.png" width="128" height="74"/>
  </tile>
  <tile id="3">
   <image source="Tiles/Tuile_chemin-4.png" width="128" height="74"/>
  </tile>
- <tile id="13">
-  <image source="Tiles/Tuile_chemin-14.png" width="128" height="74"/>
- </tile>
- <tile id="12">
-  <image source="Tiles/Tuile_chemin-13.png" width="128" height="74"/>
- </tile>
- <tile id="16">
-  <image source="Tiles/Tuile_chemin-17.png" width="128" height="74"/>
- </tile>
  <tile id="4">
   <image source="Tiles/Tuile_chemin-5.png" width="128" height="74"/>
- </tile>
- <tile id="15">
-  <image source="Tiles/Tuile_chemin-16.png" width="128" height="74"/>
- </tile>
- <tile id="19">
-  <image source="Tiles/Tuile_chemin-20.png" width="128" height="74"/>
- </tile>
- <tile id="18">
-  <image source="Tiles/Tuile_chemin-19.png" width="128" height="74"/>
- </tile>
- <tile id="6">
-  <image source="Tiles/Tuile_chemin-7.png" width="128" height="74"/>
- </tile>
- <tile id="0">
-  <image source="Tiles/Tuile_chemin-1.png" width="128" height="74"/>
  </tile>
  <tile id="5">
   <image source="Tiles/Tuile_chemin-6.png" width="128" height="74"/>
  </tile>
- <tile id="2">
-  <image source="Tiles/Tuile_chemin-3.png" width="128" height="74"/>
+ <tile id="6">
+  <image source="Tiles/Tuile_chemin-7.png" width="128" height="74"/>
  </tile>
  <tile id="7">
   <image source="Tiles/Tuile_chemin-8.png" width="128" height="74"/>
@@ -52,17 +28,41 @@
  <tile id="8">
   <image source="Tiles/Tuile_chemin-9.png" width="128" height="74"/>
  </tile>
- <tile id="20">
-  <image source="Tiles/Tuile_chemin-21.png" width="128" height="74"/>
+ <tile id="9">
+  <image source="Tiles/Tuile_chemin-10.png" width="128" height="74"/>
+ </tile>
+ <tile id="10">
+  <image source="Tiles/Tuile_chemin-11.png" width="128" height="74"/>
  </tile>
  <tile id="11">
   <image source="Tiles/Tuile_chemin-12.png" width="128" height="74"/>
  </tile>
- <tile id="17">
-  <image source="Tiles/Tuile_chemin-18.png" width="128" height="74"/>
+ <tile id="12">
+  <image source="Tiles/Tuile_chemin-13.png" width="128" height="74"/>
+ </tile>
+ <tile id="13">
+  <image source="Tiles/Tuile_chemin-14.png" width="128" height="74"/>
  </tile>
  <tile id="14">
   <image source="Tiles/Tuile_chemin-15.png" width="128" height="74"/>
+ </tile>
+ <tile id="15">
+  <image source="Tiles/Tuile_chemin-16.png" width="128" height="74"/>
+ </tile>
+ <tile id="16">
+  <image source="Tiles/Tuile_chemin-17.png" width="128" height="74"/>
+ </tile>
+ <tile id="17">
+  <image source="Tiles/Tuile_chemin-18.png" width="128" height="74"/>
+ </tile>
+ <tile id="18">
+  <image source="Tiles/Tuile_chemin-19.png" width="128" height="74"/>
+ </tile>
+ <tile id="19">
+  <image source="Tiles/Tuile_chemin-20.png" width="128" height="74"/>
+ </tile>
+ <tile id="20">
+  <image source="Tiles/Tuile_chemin-21.png" width="128" height="74"/>
  </tile>
  <tile id="21">
   <image source="Tiles/Tuile_chemin-22.png" width="128" height="74"/>
