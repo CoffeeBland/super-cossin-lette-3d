2e5c1dafd58f033906d52cb327e12357a6fae0c6
Lamouchemasquee <jessicomics@gmail.com>
2025-08-31 09:58:48 -0400

    plus PLUS de tuiles!

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -1,2 +1,407 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="128" tileheight="74" tilecount="119" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="96">
+  <image source="../img/Tiles/Tuile_herbe-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="97">
+  <image source="../img/Tiles/Tuile_herbe-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="1">
+  <image source="../img/Tiles/Tuile_chemin-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="2">
+  <image source="../img/Tiles/Tuile_chemin-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="3">
+  <image source="../img/Tiles/Tuile_chemin-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="4">
+  <image source="../img/Tiles/Tuile_chemin-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="5">
+  <image source="../img/Tiles/Tuile_chemin-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="6">
+  <image source="../img/Tiles/Tuile_chemin-6.png" width="128" height="74"/>
+ </tile>
+ <tile id="7">
+  <image source="../img/Tiles/Tuile_chemin-7.png" width="128" height="74"/>
+ </tile>
+ <tile id="8">
+  <image source="../img/Tiles/Tuile_chemin-8.png" width="128" height="74"/>
+ </tile>
+ <tile id="9">
+  <image source="../img/Tiles/Tuile_chemin-9.png" width="128" height="74"/>
+ </tile>
+ <tile id="10">
+  <image source="../img/Tiles/Tuile_chemin-10.png" width="128" height="74"/>
+ </tile>
+ <tile id="11">
+  <image source="../img/Tiles/Tuile_chemin-11.png" width="128" height="74"/>
+ </tile>
+ <tile id="12">
+  <image source="../img/Tiles/Tuile_chemin-12.png" width="128" height="74"/>
+ </tile>
+ <tile id="13">
+  <image source="../img/Tiles/Tuile_chemin-13.png" width="128" height="74"/>
+ </tile>
+ <tile id="14">
+  <image source="../img/Tiles/Tuile_chemin-14.png" width="128" height="74"/>
+ </tile>
+ <tile id="15">
+  <image source="../img/Tiles/Tuile_chemin-15.png" width="128" height="74"/>
+ </tile>
+ <tile id="16">
+  <image source="../img/Tiles/Tuile_chemin-16.png" width="128" height="74"/>
+ </tile>
+ <tile id="17">
+  <image source="../img/Tiles/Tuile_chemin-17.png" width="128" height="74"/>
+ </tile>
+ <tile id="18">
+  <image source="../img/Tiles/Tuile_chemin-18.png" width="128" height="74"/>
+ </tile>
+ <tile id="19">
+  <image source="../img/Tiles/Tuile_chemin-19.png" width="128" height="74"/>
+ </tile>
+ <tile id="20">
+  <image source="../img/Tiles/Tuile_chemin-20.png" width="128" height="74"/>
+ </tile>
+ <tile id="21">
+  <image source="../img/Tiles/Tuile_chemin-21.png" width="128" height="74"/>
+ </tile>
+ <tile id="98">
+  <image source="../img/Tiles/Tuile_chemin_courbe-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="99">
+  <image source="../img/Tiles/Tuile_chemin_courbe-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="100">
+  <image source="../img/Tiles/Tuile_chemin_courbe-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="101">
+  <image source="../img/Tiles/Tuile_chemin_courbe-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="102">
+  <image source="../img/Tiles/Tuile_chemin_courbe-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="103">
+  <image source="../img/Tiles/Tuile_chemin_courbe-6.png" width="128" height="74"/>
+ </tile>
+ <tile id="104">
+  <image source="../img/Tiles/Tuile_chemin_courbe-7.png" width="128" height="74"/>
+ </tile>
+ <tile id="105">
+  <image source="../img/Tiles/Tuile_chemin_courbe-8.png" width="128" height="74"/>
+ </tile>
+ <tile id="106">
+  <image source="../img/Tiles/Tuile_chemin_courbe-9.png" width="128" height="74"/>
+ </tile>
+ <tile id="107">
+  <image source="../img/Tiles/Tuile_chemin_courbe-10.png" width="128" height="74"/>
+ </tile>
+ <tile id="108">
+  <image source="../img/Tiles/Tuile_chemin_courbe-11.png" width="128" height="74"/>
+ </tile>
+ <tile id="109">
+  <image source="../img/Tiles/Tuile_chemin_courbe-12.png" width="128" height="74"/>
+ </tile>
+ <tile id="110">
+  <image source="../img/Tiles/Tuile_chemin_courbe-13.png" width="128" height="74"/>
+ </tile>
+ <tile id="111">
+  <image source="../img/Tiles/Tuile_chemin_courbe-14.png" width="128" height="74"/>
+ </tile>
+ <tile id="112">
+  <image source="../img/Tiles/Tuile_chemin_courbe-15.png" width="128" height="74"/>
+ </tile>
+ <tile id="113">
+  <image source="../img/Tiles/Tuile_chemin_courbe-16.png" width="128" height="74"/>
+ </tile>
+ <tile id="114">
+  <image source="../img/Tiles/Tuile_chemin_courbe-17.png" width="128" height="74"/>
+ </tile>
+ <tile id="115">
+  <image source="../img/Tiles/Tuile_chemin_courbe-18.png" width="128" height="74"/>
+ </tile>
+ <tile id="116">
+  <image source="../img/Tiles/Tuile_chemin_courbe-19.png" width="128" height="74"/>
+ </tile>
+ <tile id="117">
+  <image source="../img/Tiles/Tuile_chemin_courbe-20.png" width="128" height="74"/>
+ </tile>
+ <tile id="118">
+  <image source="../img/Tiles/Tuile_chemin_courbe-21.png" width="128" height="74"/>
+ </tile>
+ <tile id="119">
+  <image source="../img/Tiles/Tuile_chemin_courbe-22.png" width="128" height="74"/>
+ </tile>
+ <tile id="120">
+  <image source="../img/Tiles/Tuile_chemin_courbe-23.png" width="128" height="74"/>
+ </tile>
+ <tile id="121">
+  <image source="../img/Tiles/Tuile_chemin_courbe-24.png" width="128" height="74"/>
+ </tile>
+ <tile id="22">
+  <image source="../img/Tiles/Tuile_chemin-22.png" width="128" height="74"/>
+ </tile>
+ <tile id="23">
+  <image source="../img/Tiles/Tuile_chemin-23.png" width="128" height="74"/>
+ </tile>
+ <tile id="24">
+  <image source="../img/Tiles/Tuile_chemin-24.png" width="128" height="74"/>
+ </tile>
+ <tile id="25">
+  <image source="../img/Tiles/Tuile_chemin-25.png" width="128" height="74"/>
+ </tile>
+ <tile id="26">
+  <image source="../img/Tiles/Tuile_chemin-26.png" width="128" height="74"/>
+ </tile>
+ <tile id="27">
+  <image source="../img/Tiles/Tuile_chemin-27.png" width="128" height="74"/>
+ </tile>
+ <tile id="28">
+  <image source="../img/Tiles/Tuile_chemin-28.png" width="128" height="74"/>
+ </tile>
+ <tile id="29">
+  <image source="../img/Tiles/Tuile_chemin-29.png" width="128" height="74"/>
+ </tile>
+ <tile id="30">
+  <image source="../img/Tiles/Tuile_chemin-30.png" width="128" height="74"/>
+ </tile>
+ <tile id="31">
+  <image source="../img/Tiles/Tuile_eau_courbe-1.png" width="128" height="74"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="1.5" y="41.5">
+    <polygon points="0,0 103.5,-22.5 133.5,-6 61,36.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <image source="../img/Tiles/Tuile_eau_courbe-2.png" width="128" height="74"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3.5" y="42.5">
+    <polygon points="0,0 69.5,-41.5 129,-6.5 60.5,34.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <image source="../img/Tiles/Tuile_eau_courbe-3.png" width="128" height="74"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="68" y="0">
+    <polygon points="0,0 -37,59.5 -4,77.5 67,34.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <image source="../img/Tiles/Tuile_eau_courbe-4.png" width="128" height="74"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="104" y="16.5">
+    <polygon points="0,0 0.5,38 28,20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <image source="../img/Tiles/Tuile_eau_courbe-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="36">
+  <image source="../img/Tiles/Tuile_eau_courbe-6.png" width="128" height="74"/>
+ </tile>
+ <tile id="37">
+  <image source="../img/Tiles/Tuile_eau_courbe-7.png" width="128" height="74"/>
+ </tile>
+ <tile id="38">
+  <image source="../img/Tiles/Tuile_eau_courbe-8.png" width="128" height="74"/>
+ </tile>
+ <tile id="39">
+  <image source="../img/Tiles/Tuile_eau_courbe-9.png" width="128" height="74"/>
+ </tile>
+ <tile id="40">
+  <image source="../img/Tiles/Tuile_eau_courbe-10.png" width="128" height="74"/>
+ </tile>
+ <tile id="41">
+  <image source="../img/Tiles/Tuile_eau_courbe-11.png" width="128" height="74"/>
+ </tile>
+ <tile id="42">
+  <image source="../img/Tiles/Tuile_eau_courbe-12.png" width="128" height="74"/>
+ </tile>
+ <tile id="43">
+  <image source="../img/Tiles/Tuile_eau_courbe-13.png" width="128" height="74"/>
+ </tile>
+ <tile id="44">
+  <image source="../img/Tiles/Tuile_eau_courbe-14.png" width="128" height="74"/>
+ </tile>
+ <tile id="45">
+  <image source="../img/Tiles/Tuile_eau_courbe-15.png" width="128" height="74"/>
+ </tile>
+ <tile id="46">
+  <image source="../img/Tiles/Tuile_eau_courbe-16.png" width="128" height="74"/>
+ </tile>
+ <tile id="47">
+  <image source="../img/Tiles/Tuile_eau_courbe-17.png" width="128" height="74"/>
+ </tile>
+ <tile id="48">
+  <image source="../img/Tiles/Tuile_eau_courbe-18.png" width="128" height="74"/>
+ </tile>
+ <tile id="49">
+  <image source="../img/Tiles/Tuile_eau_courbe-19.png" width="128" height="74"/>
+ </tile>
+ <tile id="50">
+  <image source="../img/Tiles/Tuile_eau_courbe-20.png" width="128" height="74"/>
+ </tile>
+ <tile id="51">
+  <image source="../img/Tiles/Tuile_eau_courbe-21.png" width="128" height="74"/>
+ </tile>
+ <tile id="52">
+  <image source="../img/Tiles/Tuile_eau_courbe-22.png" width="128" height="74"/>
+ </tile>
+ <tile id="53">
+  <image source="../img/Tiles/Tuile_eau_courbe-23.png" width="128" height="74"/>
+ </tile>
+ <tile id="54">
+  <image source="../img/Tiles/Tuile_eau_courbe-24.png" width="128" height="74"/>
+ </tile>
+ <tile id="95">
+  <image source="../img/Tiles/Tuile_eau-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="56">
+  <image source="../img/Tiles/Tuile_eau-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="57">
+  <image source="../img/Tiles/Tuile_eau-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="58">
+  <image source="../img/Tiles/Tuile_eau-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="59">
+  <image source="../img/Tiles/Tuile_eau-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="60">
+  <image source="../img/Tiles/Tuile_eau-6.png" width="128" height="74"/>
+ </tile>
+ <tile id="61">
+  <image source="../img/Tiles/Tuile_eau-7.png" width="128" height="74"/>
+ </tile>
+ <tile id="62">
+  <image source="../img/Tiles/Tuile_eau-8.png" width="128" height="74"/>
+ </tile>
+ <tile id="63">
+  <image source="../img/Tiles/Tuile_eau-9.png" width="128" height="74"/>
+ </tile>
+ <tile id="64">
+  <image source="../img/Tiles/Tuile_eau-10.png" width="128" height="74"/>
+ </tile>
+ <tile id="65">
+  <image source="../img/Tiles/Tuile_eau-11.png" width="128" height="74"/>
+ </tile>
+ <tile id="66">
+  <image source="../img/Tiles/Tuile_eau-12.png" width="128" height="74"/>
+ </tile>
+ <tile id="67">
+  <image source="../img/Tiles/Tuile_eau-13.png" width="128" height="74"/>
+ </tile>
+ <tile id="68">
+  <image source="../img/Tiles/Tuile_eau-14.png" width="128" height="74"/>
+ </tile>
+ <tile id="69">
+  <image source="../img/Tiles/Tuile_eau-15.png" width="128" height="74"/>
+ </tile>
+ <tile id="70">
+  <image source="../img/Tiles/Tuile_eau-16.png" width="128" height="74"/>
+ </tile>
+ <tile id="71">
+  <image source="../img/Tiles/Tuile_eau-17.png" width="128" height="74"/>
+ </tile>
+ <tile id="72">
+  <image source="../img/Tiles/Tuile_eau-18.png" width="128" height="74"/>
+ </tile>
+ <tile id="73">
+  <image source="../img/Tiles/Tuile_eau-19.png" width="128" height="74"/>
+ </tile>
+ <tile id="74">
+  <image source="../img/Tiles/Tuile_eau-20.png" width="128" height="74"/>
+ </tile>
+ <tile id="75">
+  <image source="../img/Tiles/Tuile_eau-21.png" width="128" height="74"/>
+ </tile>
+ <tile id="76">
+  <image source="../img/Tiles/Tuile_eau-22.png" width="128" height="74"/>
+ </tile>
+ <tile id="77">
+  <image source="../img/Tiles/Tuile_eau-23.png" width="128" height="74"/>
+ </tile>
+ <tile id="78">
+  <image source="../img/Tiles/Tuile_eau-24.png" width="128" height="74"/>
+ </tile>
+ <tile id="79">
+  <image source="../img/Tiles/Tuile_eau-25.png" width="128" height="74"/>
+ </tile>
+ <tile id="80">
+  <image source="../img/Tiles/Tuile_eau-26.png" width="128" height="74"/>
+ </tile>
+ <tile id="81">
+  <image source="../img/Tiles/Tuile_eau-27.png" width="128" height="74"/>
+ </tile>
+ <tile id="82">
+  <image source="../img/Tiles/Tuile_eau-28.png" width="128" height="74"/>
+ </tile>
+ <tile id="83">
+  <image source="../img/Tiles/Tuile_eau-29.png" width="128" height="74"/>
+  <animation>
+   <frame tileid="83" duration="200"/>
+   <frame tileid="82" duration="200"/>
+   <frame tileid="81" duration="200"/>
+   <frame tileid="80" duration="200"/>
+   <frame tileid="81" duration="200"/>
+   <frame tileid="82" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <image source="../img/Tiles/Tuile_eau-30.png" width="128" height="74"/>
+ </tile>
+ <tile id="85">
+  <image source="../img/Tiles/Tuile_eau-31.png" width="128" height="74"/>
+ </tile>
+ <tile id="86">
+  <image source="../img/Tiles/Tuile_eau-32.png" width="128" height="74"/>
+ </tile>
+ <tile id="87">
+  <image source="../img/Tiles/Tuile_eau-33.png" width="128" height="74"/>
+  <animation>
+   <frame tileid="87" duration="200"/>
+   <frame tileid="86" duration="200"/>
+   <frame tileid="85" duration="200"/>
+   <frame tileid="84" duration="200"/>
+   <frame tileid="85" duration="200"/>
+   <frame tileid="86" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="88">
+  <image source="../img/Tiles/Tuile_eau-34.png" width="128" height="74"/>
+ </tile>
+ <tile id="89">
+  <image source="../img/Tiles/Tuile_eau-35.png" width="128" height="74"/>
+ </tile>
+ <tile id="90">
+  <image source="../img/Tiles/Tuile_eau-36.png" width="128" height="74"/>
+  <animation>
+   <frame tileid="88" duration="200"/>
+   <frame tileid="89" duration="200"/>
+   <frame tileid="90" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="91">
+  <image source="../img/Tiles/Tuile_eau-37.png" width="128" height="74"/>
+ </tile>
+ <tile id="92">
+  <image source="../img/Tiles/Tuile_eau-38.png" width="128" height="74"/>
+ </tile>
+ <tile id="93">
+  <image source="../img/Tiles/Tuile_eau-39.png" width="128" height="74"/>
+  <animation>
+   <frame tileid="91" duration="200"/>
+   <frame tileid="92" duration="200"/>
+   <frame tileid="93" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
