752d80b829fcc37b3348be1845c8b04dd60b38a6
Annie-Claude <tremblay.annieclaude@gmail.com>
2025-08-30 21:04:45 -0400

    GNA

--- tiled/Fruits.tsx
+++ tiled/Fruits.tsx
@@ -2,7 +2,7 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Fruits" tilewidth="128" tileheight="192" tilecount="11" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../img/Pomme.png" width="116" height="115"/>
+  <image source="../img/Pomme.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="348" y="920">
     <ellipse/>
@@ -16,33 +16,33 @@
   </objectgroup>
  </tile>
  <tile id="1">
-  <image source="../img/Fruits/Fruit-1.png" width="128" height="192"/>
+  <image source="../img/Fruit-1.png" width="128" height="192"/>
  </tile>
  <tile id="2">
-  <image source="../img/Fruits/Fruit-2.png" width="128" height="192"/>
+  <image source="../img/Fruit-2.png" width="128" height="192"/>
  </tile>
  <tile id="3">
-  <image source="../img/Fruits/Fruit-3.png" width="128" height="192"/>
+  <image source="../img/Fruit-3.png" width="128" height="192"/>
  </tile>
  <tile id="4">
-  <image source="../img/Fruits/Fruit-4.png" width="128" height="192"/>
+  <image source="../img/Fruit-4.png" width="128" height="192"/>
  </tile>
  <tile id="5">
-  <image source="../img/Fruits/Fruit-5.png" width="128" height="192"/>
+  <image source="../img/Fruit-5.png" width="128" height="192"/>
  </tile>
  <tile id="6">
-  <image source="../img/Fruits/Fruit-6.png" width="128" height="192"/>
+  <image source="../img/Fruit-6.png" width="128" height="192"/>
  </tile>
  <tile id="7">
-  <image source="../img/Fruits/Fruit-7.png" width="128" height="192"/>
+  <image source="../img/Fruit-7.png" width="128" height="192"/>
  </tile>
  <tile id="8">
-  <image source="../img/Fruits/Fruit-8.png" width="128" height="192"/>
+  <image source="../img/Fruit-8.png" width="128" height="192"/>
  </tile>
  <tile id="9">
-  <image source="../img/Fruits/Fruit-9.png" width="128" height="192"/>
+  <image source="../img/Fruit-9.png" width="128" height="192"/>
  </tile>
  <tile id="10">
-  <image source="../img/Fruits/Fruit-10.png" width="128" height="192"/>
+  <image source="../img/Fruit-10.png" width="128" height="192"/>
  </tile>
 </tileset>
