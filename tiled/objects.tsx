03eedf72708b2092e7381318ab08a2d87042d990
Annie-Claude <tremblay.annieclaude@gmail.com>
2025-08-30 20:15:22 -0400

    Fixed tsx
    WOW Part 2

--- tiled/objects.tsx
+++ tiled/objects.tsx
@@ -1,73 +1,233 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="objects" tilewidth="1152" tileheight="1286" tilecount="8" columns="0" objectalignment="topleft">
- <editorsettings>
-  <export target="../data/objects.lua" format="lua"/>
- </editorsettings>
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="1152" tileheight="1286" tilecount="68" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <properties>
-   <property name="offsetX" type="float" value="280"/>
-   <property name="offsetY" type="float" value="560"/>
-  </properties>
   <image source="../img/Arche_arriere.png" width="1152" height="1024"/>
   <objectgroup draworder="index" id="3">
-   <object id="2" x="309" y="452">
-    <polygon points="2,-26 188,91 -82,235 -271,133"/>
+   <object id="6" x="39.3223" y="578.27">
+    <polygon points="0,0 277.569,-152.663 451.05,-46.2616 451.05,-34.6962 418.667,-6.93924 198.925,104.089 120.28,83.2708 57.827,53.2008"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1">
-  <properties>
-   <property name="offsetX" type="float" value="860"/>
-   <property name="offsetY" type="float" value="830"/>
-  </properties>
   <image source="../img/Arche_avant.png" width="1152" height="1024"/>
   <objectgroup draworder="index" id="2">
-   <object id="1" x="568" y="888.667">
-    <polygon points="16,0 217,100.333 549,-72 342,-188.667"/>
+   <object id="1" x="575.957" y="878.97">
+    <polygon points="0,0 340.023,-171.168 534.321,-71.7054 541.26,-64.7662 240.56,99.4624 157.289,94.8362"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="2">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1220"/>
-  </properties>
   <image source="../img/Feuillage1.png" width="1024" height="1286"/>
  </tile>
  <tile id="3">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1220"/>
-  </properties>
   <image source="../img/Feuillage2.png" width="1024" height="1286"/>
  </tile>
  <tile id="4">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1220"/>
-  </properties>
   <image source="../img/Feuillage3.png" width="1024" height="1286"/>
  </tile>
  <tile id="5">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1210"/>
-  </properties>
   <image source="../img/Tronc1.png" width="1024" height="1286"/>
+  <objectgroup draworder="index" id="4">
+   <object id="5" x="381.658" y="1200.49" width="240.56" height="37.0093">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="6">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1210"/>
-  </properties>
   <image source="../img/Tronc2.png" width="1024" height="1286"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="434.859" y="1195.86" width="215.116" height="39.3223">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="7">
-  <properties>
-   <property name="offsetX" type="float" value="510"/>
-   <property name="offsetY" type="float" value="1210"/>
-  </properties>
   <image source="../img/Tronc3.png" width="1024" height="1286"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="367.78" y="1188.92" width="254.439" height="60.14">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Arche_arriere.png" width="1152" height="1024"/>
+ </tile>
+ <tile id="9">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Arche_avant.png" width="1152" height="1024"/>
+ </tile>
+ <tile id="10">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_grand-1.png" width="384" height="216"/>
+ </tile>
+ <tile id="11">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_grand-2.png" width="384" height="216"/>
+ </tile>
+ <tile id="12">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_moyen-1.png" width="256" height="148"/>
+ </tile>
+ <tile id="13">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_moyen-2.png" width="256" height="148"/>
+ </tile>
+ <tile id="14">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_petit-1.png" width="128" height="148"/>
+ </tile>
+ <tile id="15">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_petit-2.png" width="128" height="148"/>
+ </tile>
+ <tile id="16">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_petit-3.png" width="128" height="148"/>
+ </tile>
+ <tile id="17">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_petit-4.png" width="128" height="148"/>
+ </tile>
+ <tile id="18">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Buisson_petit-5.png" width="128" height="148"/>
+ </tile>
+ <tile id="19">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Champignons-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="20">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Champignons-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="21">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Champignons-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="22">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Champignons-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="24">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/cossin.png" width="960" height="512"/>
+ </tile>
+ <tile id="29">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Feuillage1.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="30">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Feuillage2.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="31">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Feuillage3.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="32">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-1.png" width="128" height="192"/>
+ </tile>
+ <tile id="33">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-2.png" width="128" height="192"/>
+ </tile>
+ <tile id="34">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-3.png" width="128" height="192"/>
+ </tile>
+ <tile id="35">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-4.png" width="128" height="192"/>
+ </tile>
+ <tile id="36">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-5.png" width="128" height="192"/>
+ </tile>
+ <tile id="37">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-6.png" width="128" height="192"/>
+ </tile>
+ <tile id="38">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-7.png" width="128" height="192"/>
+ </tile>
+ <tile id="39">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-8.png" width="128" height="192"/>
+ </tile>
+ <tile id="40">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-9.png" width="128" height="192"/>
+ </tile>
+ <tile id="41">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-10.png" width="128" height="192"/>
+ </tile>
+ <tile id="42">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Fruit-11.png" width="128" height="192"/>
+ </tile>
+ <tile id="43">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_mini-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="44">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_mini-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="45">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_mini-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="46">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_mini-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="47">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-1.png" width="128" height="148"/>
+ </tile>
+ <tile id="48">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-2.png" width="128" height="148"/>
+ </tile>
+ <tile id="49">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-3.png" width="128" height="148"/>
+ </tile>
+ <tile id="50">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-4.png" width="128" height="148"/>
+ </tile>
+ <tile id="51">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-5.png" width="128" height="148"/>
+ </tile>
+ <tile id="52">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_moyenne-6.png" width="128" height="148"/>
+ </tile>
+ <tile id="53">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_petit-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="54">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_petit-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="55">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_petit-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="56">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_petit-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="57">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Roche_petit-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="58">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/tileset.png" width="512" height="296"/>
+ </tile>
+ <tile id="59">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tronc1.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="60">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tronc2.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="61">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tronc3.png" width="1024" height="1286"/>
+ </tile>
+ <tile id="62">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_large.png" width="256" height="148"/>
+ </tile>
+ <tile id="63">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_moyen.png" width="128" height="72"/>
+ </tile>
+ <tile id="64">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-1.png" width="128" height="74"/>
+ </tile>
+ <tile id="65">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-2.png" width="128" height="74"/>
+ </tile>
+ <tile id="66">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-3.png" width="128" height="74"/>
+ </tile>
+ <tile id="67">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-4.png" width="128" height="74"/>
+ </tile>
+ <tile id="68">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-5.png" width="128" height="74"/>
+ </tile>
+ <tile id="69">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-6.png" width="128" height="74"/>
+ </tile>
+ <tile id="70">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_petit-7.png" width="128" height="74"/>
+ </tile>
+ <tile id="71">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/Tuile_roche_ultra.png" width="256" height="148"/>
+ </tile>
+ <tile id="73">
+  <image source="C:/Users/Lenovo/Documents/GitHub/super-cossin-lette-3d/img/vieuxCossinShadow.png" width="48" height="48"/>
  </tile>
 </tileset>
