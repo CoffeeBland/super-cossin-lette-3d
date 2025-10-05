daf11e1d800cd1ae851939124e34aa3f3ff9c628
Annie-Claude <tremblay.annieclaude@gmail.com>
2025-10-05 15:20:27 -0400

    Niveau 2 progress

--- tiled/objects.tsx
+++ tiled/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="objects" tilewidth="1152" tileheight="1286" tilecount="77" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="1152" tileheight="1286" tilecount="77" columns="0">
  <editorsettings>
   <export target="../data/objects.lua" format="lua"/>
  </editorsettings>
